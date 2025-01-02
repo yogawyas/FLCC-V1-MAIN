bb8a483982af9b3dd678f272046f4dae4c01455c
gemesusi <charliemartin190904@gmail.com>
2025-01-02 13:32:22 +0700

    minsry

--- resources/views/ministry/index.blade.php
+++ resources/views/ministry/index.blade.php
@@ -236,7 +236,15 @@
                     </div>
                 </div>
                 @empty
-                <!-- [Empty state remains the same] -->
+                <div class="col-span-full text-center py-12">
+                    <div class="text-gray-500 dark:text-gray-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 mx-auto mb-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+                        </svg>
+                        <p class="text-xl">No ministries available at the moment.</p>
+                        <p class="mt-2">Please check back later for updates.</p>
+                    </div>
+                </div>
                 @endforelse
             </div>
         </div>
